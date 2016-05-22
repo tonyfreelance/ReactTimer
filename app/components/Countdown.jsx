dcc58bb210e3cd3f9c2db0a5d3b60e50e4a9873b
Tony Nguyen <tony@tonyfreelance.com>
2016-05-22 03:01:01 +0000

    Update status to stopped when count reaches zero

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -16,14 +16,21 @@ var Countdown = React.createClass({
 				case 'started':
 					this.startTimer();
 					break;
-				case 'stopped': 
-					this.setState({count: 0});
-				case 'paused': 
+				case 'stopped':
+					this.setState({
+						count: 0
+					});
+				case 'paused':
 					clearInterval(this.timer);
 					this.timer = undefined;
 					break;
 			}
 		}
+	},
+	componentWillUnmount: function() {
+		clearInterval(this.timer);
+		this.timer = undefined;
+
 	},
 	startTimer: function() {
 		this.timer = setInterval(() => {
@@ -31,6 +38,12 @@ var Countdown = React.createClass({
 			this.setState({
 				count: newCount >= 0 ? newCount : 0
 			});
+
+			if (newCount === 0) {
+				this.setState({
+					countdownStatus: 'stopped'
+				});
+			}
 		}, 1000);
 	},
 	handleSetCountdown: function(seconds) {
